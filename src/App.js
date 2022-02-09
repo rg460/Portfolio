a8fa23cce0f708737ef6f3a3d3349f0f13628116
ravigordon <rgordz460@outlook.com>
2022-02-09 18:18:28 +0000

    folder sctructure and react routes set up for portfolio

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import {render} from "react-dom";
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import './App.css';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <div className="App">
+        
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+
+    </div>
+  );
+}
+
+
+export default App;
