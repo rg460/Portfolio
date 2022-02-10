41d925a5212539b8121f1e78d511f8b74d664741
ravigordon <rgordz460@outlook.com>
2022-02-10 18:51:58 +0000

    onclick event created to change hamburger menu to cross when clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
-import React,{useState} from 'react'
-import { RiMenu3Fill } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
 
-function Header(){
-    // create a state when clicking on hamburger menu header is shown 
-    const[showHeader,setShowHeader] = useState(false)
-    return(
-        <div className="header">
-             
-             <RiMenu3Fill className="menu-icon position-fixed  top-0 end-0" />
-
-        </div>
-    )
+function Header() {
+  // create a state when clicking on hamburger menu header is shown
+  const [showHeader, setShowHeader] = useState(false);
+  return (
+    <div className="header">
+      {/* if showHeader is true show cross else show header is false then show handburger */}
+      {showHeader ? (
+        <RiCloseFill
+          //   write onclick event for RiCloseFill
+          onClick={() =>{setShowHeader(!showHeader)}}
+          className="menu-icon position-fixed  top-0 end-0"
+        />
+      ) : (
+        <RiMenu3Fill className="menu-icon position-fixed  top-0 end-0" //   write onclick event for RiCloseFill
+          onClick={() =>{setShowHeader(!showHeader)}} />
+      )}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
