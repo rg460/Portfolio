ae487a74b201fd2e9b155b0d356d57df4ede17af
ravigordon <rgordz460@outlook.com>
2022-02-10 12:49:43 +0000

    header section styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ import React from 'react'
               <h1>RAVI GORDON</h1>
               <div className="intro-content d-flex justify-content-between">
                  <p>Front End Web Developer<br /> Full Stack Javascript Developer<br /> Student at The School of Code<br />looking to chnage my life through code</p>
-                 <button className='primary-button'>Get Started</button> 
+                 <button className='primary-button font-bold w-50'>Get Started</button> 
               </div>
           </div>
         </div>
