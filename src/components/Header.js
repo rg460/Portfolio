4b0adbace1ba612fc3dd4b4bb538eabf76f29f19
ravigordon <rgordz460@outlook.com>
2022-02-10 15:10:57 +0000

    react icons installed and useState created for header hamburger menu

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+
+
+function Header(){
+    // create a state when clicking on hamburger menu header is shown 
+    const[showHeader,setShowHeader] = useState(false)
+    return(
+        <div className="header">
+             {/* create hamburger icon */}
+             
+        </div>
+    )
+}
+
+export default Header
