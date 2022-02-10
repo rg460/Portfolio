cc276ab03ea3fadbfd5ec6b07bc1c44c5fe9a0e5
ravigordon <rgordz460@outlook.com>
2022-02-10 17:13:21 +0000

    burger icon added to header u sinng react-icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React,{useState} from 'react'
-
+import { RiMenu3Fill } from "react-icons/ri";
 
 function Header(){
     // create a state when clicking on hamburger menu header is shown 
     const[showHeader,setShowHeader] = useState(false)
     return(
         <div className="header">
-             {/* create hamburger icon */}
              
+             <RiMenu3Fill />
+
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
  function Home() {
   return (
     <div>
+       <Header /> 
+
         <div className="introduction flex-with-center" style={{backgroundImage: `url('./introbg.svg')`}}>
           <div>
               <h1>RAVI GORDON</h1>
