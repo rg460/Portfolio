e73a605c4e281790399e96cf85963756d652c6d2
ravigordon <rgordz460@outlook.com>
2022-02-11 11:35:56 +0000

    colour of selected page text colour on drop down navbar changed to white

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,38 +2,23 @@ import React, { useState } from "react";
 import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 function Header() {
-  // create a state when clicking on hamburger menu header is shown
   const [showHeader, setShowHeader] = useState(false);
+  const path = window.location.pathname
   return (
     <div className="header">
-      {/* if showHeader is true show cross else show header is false then show handburger */}
       {showHeader ? (
-        <RiCloseFill
-          //   write onclick event for RiCloseFill
-          onClick={() => {
-            setShowHeader(!showHeader);
-          }}
-          className="menu-icon position-fixed  top-0 end-0"
-        />
+        <RiCloseFill 
+         onClick={()=>{setShowHeader(!showHeader)}}
+        className="menu-icon position-fixed top-0 end-0" />
       ) : (
-        <RiMenu3Fill
-          className="menu-icon position-fixed  top-0 end-0" //   write onclick event for RiCloseFill
-          onClick={() => {
-            setShowHeader(!showHeader);
-          }}
-        />
+        <RiMenu3Fill className="menu-icon position-fixed top-0 end-0" onClick={()=>{setShowHeader(!showHeader)}}/>
       )}
-      {/* if showHeader is true then go apply show-header class if false apply hide-header class */}
-      <ul className={`${showHeader ? "show-header" : "hide-header"} n-box1`}> 
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/Contact">Contact</Link>
-        </li>
+        {/* if showHeader is true then go apply show-header class if false apply hide-header class */}
+      <ul className={`${showHeader ? 'show-header' : 'hide-header'} n-box1`}>
+          {/* if Path name is equal to active then change the border to white */}
+          <li className={`${path=='/' && 'active'}`}><Link to='/'>Home</Link></li>
+          <li className={`${path=='/projects' && 'active'}`}><Link to='projects'>Projects</Link></li>
+          <li className={`${path=='/contact' && 'active'}`}><Link to='/contact'>Contact</Link></li>
       </ul>
     </div>
   );
